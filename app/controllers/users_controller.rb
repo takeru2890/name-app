2c5037f6f4662af96e1bfd220be2bbd5d19ceddf
EC2 Default User <ec2-user@ip-172-31-29-109.us-east-2.compute.internal>
2020-07-14 12:12:09 +0000

    change url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     
     def create
         if  User.create(name: params["user"]["user_name"],age:  params["user"]["age"],mail: params["user"]["mail"]).valid?
-            redirect_to "/names"
+            redirect_to "/"
         else
             render plain: "error"
         end
